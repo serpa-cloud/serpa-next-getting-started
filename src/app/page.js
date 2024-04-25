739acf7583a0d251096baff270184207d76379c2
Emilia González <emilia@serpa.cloud>
2024-04-25 14:13:12 -0600

    feat: init repo

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+      </div>
+    </main>
+  );
+}
